9ac6d27a740d4d0c2bc9028a48477e506eca6021
kouwt <107230335+kouwt@users.noreply.github.com>
2025-03-24 21:28:36 +0800

    Initial commit

---
+++ src/mastra/index.ts
@@ -0,0 +1,42 @@
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
+import { cursorRulesWorkflow } from "./workflows";
+import { cursorRulesAgent } from "./agents";
+
+
+export const mastra = new Mastra({
+    agents: {
+        cursorRulesAgent,
+    },
+    workflows: {
+        cursorRulesWorkflow,
+    },
+    logger: createLogger({
+        name: "GitHub Cursor Rules Agent",
+        level: "debug",
+    }),
+});
+
+// const mastra1 = new Mastra({
+//     workflows: {
+//         cursorRulesWorkflow,
+//     },
+//   });
+
+// (async () => {
+//     const { runId, start } = mastra1
+//         .getWorkflow("cursorRulesWorkflow")
+//         .createRun();
+
+//     console.log("Run", runId);
+
+//     const runResult = await start({
+//         triggerData: {
+//             repositoryUrl: "https://github.com/honojs/hono",
+//             branch: "main",
+//             outputPath: "./examples/rules",
+//         },
+//     });
+
+//     console.log("Final output:", runResult.results);
+// })();
